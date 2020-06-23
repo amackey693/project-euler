980781560a26065ba92c0313f2d602f56ab66f09
Allison Mackey <a.mackey693@gmail.com>
2020-06-23 10:31:45 -0700

    use filter & reduce to work through first project euler problem.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
-import $ from "jquery";
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './styles.css';
+// import $ from "jquery";
+// https://projecteuler.net/problem=1
+const number = 1001;
+const list = [...Array(number - 1).keys()].map(e => e + 1);
+
+const multiples = list.filter(e => (e % 3 === 0) || (e % 5 === 0));
+
+const addMultiples = multiples.reduce((c, n) => c + n, 0); 
+
